2b2ed6e748903559314ab7942059452e18d7384d
Colin Kiama <colinkiama@gmail.com>
2019-10-13 14:25:26 +0100

    Now calculating values of neighbour tiles.

--- MinesweeperNetCore/Game.cs
+++ MinesweeperNetCore/Game.cs
@@ -7,12 +7,17 @@ namespace MinesweeperNetCore
 {
     public class Game
     {
+        public const int MineValue = -1;
+        public const int BlankTileValue = 0;
+
+
         Board gameBoard = new Board();
         // Program will create this object then start a game with it. When game ends, object is destroyed 
         // => Context returns to program, which will switch the state back to none 
         internal void Start()
         {
-            throw new NotImplementedException();
+            Console.Clear();
+            gameBoard.FillBoard();
         }
     }
 }
--- MinesweeperNetCore/Model/Board.cs
+++ MinesweeperNetCore/Model/Board.cs
@@ -7,11 +7,102 @@ namespace MinesweeperNetCore.Model
     public class Board
     {
         int[,] _boardArray = new int[9, 9];
+        Random rnd = new Random();
 
+        public int Width => _boardArray.Length;
         public int this[int indexRow, int indexColumn]
         {
             get { return _boardArray[indexRow, indexColumn]; }
             set { _boardArray[indexRow, indexColumn] = value; }
+        }
+
+        internal void FillBoard()
+        {
+            GenerateMines();
+            IdentifyNeighbourTiles();
+        }
+
+        private void IdentifyNeighbourTiles()
+        {
+            for (int row = 0; row < this.Width; row++)
+            {
+                for (int column = 0; column < this.Width; column++)
+                {
+                    this[row, column] = CheckForNearbyMines(row, column);
+                }
+            }
+        }
+
+        private int CheckForNearbyMines(int row, int column)
+        {
+            int nearbyMines = 0;
+            nearbyMines += CheckForHorizontalMines(row, column);
+            nearbyMines += CheckForVerticalMines(row, column);
+            nearbyMines += CheckForDiagonalMines(row, column);
+            return nearbyMines;
+        }
+
+        private int CheckForDiagonalMines(int row, int column)
+        {
+            int mines = 0;
+            return mines;
+        }
+
+        private int CheckForVerticalMines(int row, int column)
+        {
+            int mines = 0;
+            return mines;
+        }
+
+        private int CheckForHorizontalMines(int row, int column)
+        {
+            int mines = 0;
+
+            // Left
+            int leftColumn = column - 1;
+            if (leftColumn > -1)
+            {
+                if (this[row, column] == Game.MineValue)
+                {
+                    mines += 1;
+                }
+            }
+
+
+            // Right
+            int rightColumn = column + 1;
+            if (rightColumn < this.Width)
+            {
+                if (this[row, rightColumn] == Game.MineValue)
+                {
+                    mines += 1;
+                }
+            }
+
+
+            return mines;
+        }
+
+        private void GenerateMines()
+        {
+            for (int row = 0; row < _boardArray.Length; row++)
+            {
+                for (int column = 0; column < _boardArray.Length; column++)
+                {
+                    int tileValue = GeneratedTileValue();
+                    if (tileValue == Game.MineValue)
+                    {
+                        _boardArray[row, column] = tileValue;
+                    }
+                }
+            }
+        }
+
+        private int GeneratedTileValue()
+        {
+            // Random number from -1 to random number from 6 to 10
+            // so there is a 8.33%-12.5% chance of the tile being a mine
+            return rnd.Next(-1, rnd.Next(6, 12));
         }
     }
 }
