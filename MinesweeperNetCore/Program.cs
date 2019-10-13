2290a0e8e6187a0ddb495bc52b84913a4a0bcc7b
Colin Kiama <colinkiama@gmail.com>
2019-10-13 12:37:44 +0100

    Got options loop setup, now need to actually program the options

--- MinesweeperNetCore/Program.cs
+++ MinesweeperNetCore/Program.cs
@@ -1,31 +1,65 @@
-﻿using System;
+﻿using MinesweeperNetCore.Enums;
+using System;
 
 namespace MinesweeperNetCore
 {
     class Program
     {
-        public static bool hasUserQuitProgram = false;
+        private static bool hasUserQuitProgram = false;
+        public static GameState CurrentState = GameState.None;
         static void Main(string[] args)
         {
             DisplayMainMenuOptions();
             while (!hasUserQuitProgram)
             {
+                switch (CurrentState)
+                {
+                    case GameState.MainMenu:
+                        ReadUserOptionInput();
+                        break;
+                    case GameState.None:
+                        DisplayMainMenuOptions();
+                        break;
+                    default:
+                        break;
+                }
+                
             }
+        }
+
+        private static void ReadUserOptionInput()
+        {
+            string optionInput = Console.ReadLine();
+            var treatedInput = optionInput.Trim();
+            char parsedOption;
+            bool isValidChar = char.TryParse(treatedInput, out parsedOption);
+            if (!isValidChar || !CheckIfValidOption(parsedOption))
+            {
+                DisplayInvalidOptionMessage();
+            }
+        }
+
+        private static void DisplayInvalidOptionMessage()
+        {
+            Console.WriteLine("You did not enter a valid option, please try again");
+            CurrentState = GameState.None;
+        }
+
+        private static bool CheckIfValidOption(char parsedOption)
+        {
+            return false;
         }
 
         private static void DisplayMainMenuOptions()
         {
-            int optionNumber = 1;
-            PrintOption(ref optionNumber, "This");
-            PrintOption(ref optionNumber, "is");
-            PrintOption(ref optionNumber, "a");
-            PrintOption(ref optionNumber, "Test");
+            PrintOption('1', "Start new game");
+            PrintOption('q', "Quit");
+            CurrentState = GameState.MainMenu;
         }
 
-        private static void PrintOption(ref int optionNumber, string optionDescription)
+        private static void PrintOption(char optionKey, string optionDescription)
         {
-            Console.WriteLine($"[{optionNumber}] {optionDescription}");
-            optionNumber++;
+            Console.WriteLine($"[{Char.ToUpper(optionKey)}] {optionDescription}");
         }
     }
 }
