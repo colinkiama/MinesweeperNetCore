42ccb51cef4f6de5c42d7d71e9686b504d684cb0
Colin Kiama <colinkiama@gmail.com>
2019-10-13 12:22:47 +0100

    Add project files.

---
+++ MinesweeperNetCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MinesweeperNetCore
+{
+    class Program
+    {
+        public static bool hasUserQuitProgram = false;
+        static void Main(string[] args)
+        {
+            DisplayMainMenuOptions();
+            while (!hasUserQuitProgram)
+            {
+            }
+        }
+
+        private static void DisplayMainMenuOptions()
+        {
+            int optionNumber = 1;
+            PrintOption(ref optionNumber, "This");
+            PrintOption(ref optionNumber, "is");
+            PrintOption(ref optionNumber, "a");
+            PrintOption(ref optionNumber, "Test");
+        }
+
+        private static void PrintOption(ref int optionNumber, string optionDescription)
+        {
+            Console.WriteLine($"[{optionNumber}] {optionDescription}");
+            optionNumber++;
+        }
+    }
+}
